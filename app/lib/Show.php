bf51bec655a3e881372de597bf89fb8df30d7217
Kylin <kylin87@qq.com>
2020-03-11 05:10:43 +0000

    init

---
+++ app/lib/Show.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\lib;
+/**
+ * 通知lib
+ */
+class Show
+{
+    /**
+     * 成功api通知
+     * @param  array  $data    [description]
+     * @param  string $message [description]
+     * @return [type]          [description]
+     */
+    public static function success($data = [], $message = "OK"){
+        $results = [
+            'status'    => config("status.success"),
+            'message'   => $message,
+            'result'    => $data,
+        ];
+
+        return json($results);
+    }
+
+    public static function error($message = "error",$status = 0, $data = []){
+        $results = [
+            'status'    => $status,
+            'message'   => $message,
+            'result'    => $data,
+        ];
+
+        return json($results);
+    }
+}
